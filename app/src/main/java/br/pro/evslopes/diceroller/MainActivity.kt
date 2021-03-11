c2f1c4f5bf8fbc691e8b1598cbca667020e1d14f
Elvis Lopes <elvis.lopes@al.infnet.edu.br>
2021-03-10 21:56:59 -0300

    Refatorando - Extraído funções e adicionado botão redefinir

--- app/src/main/java/br/pro/evslopes/diceroller/MainActivity.kt
+++ app/src/main/java/br/pro/evslopes/diceroller/MainActivity.kt
@@ -14,18 +14,37 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
 
+        val redefinirButton: Button = findViewById(R.id.redefinir_button)
+        redefinirButton.setOnClickListener { redefinir() }
+
+    }
+
+    private fun redefinir() {
+        atualizarResultado(0)
     }
 
     private fun rollDice(){
-        //Toast.makeText(this, "button clicked",
-        //Toast.LENGTH_LONG).show()
 
+        mensagemToasts("Dados rolando...")
         //Jogada do dado
         val randomInt = (1..6).random()
 
+        mensagemToasts("Seu resultado!")
         //Atualização do resuldo obtido na label
+        atualizarResultado(randomInt)
+
+    }
+
+    private fun atualizarResultado(randomInt: Int) {
         val resultText: TextView = findViewById(R.id.result_text)
         resultText.text = randomInt.toString()
+    }
 
+    //Funcao para exibir as msg durante as jogadas.
+    private fun mensagemToasts(msg:String) {
+        Toast.makeText(
+            this, msg,
+            Toast.LENGTH_SHORT
+        ).show()
     }
 }
